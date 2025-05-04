baa9c3e948fe89131c06a36a5eae09134324c3d9
sorivma <sorivma03.06@gmail.com>
2025-05-04 11:55:02 +0300

    initial commit

---
+++ src/main/kotlin/sorivma/geosearchindexer/GeoSearchIndexerApplication.kt
@@ -0,0 +1,11 @@
+package sorivma.geosearchindexer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GeoSearchIndexerApplication
+
+fun main(args: Array<String>) {
+    runApplication<GeoSearchIndexerApplication>(*args)
+}
